43c185d04133131735e7aa3bbd0fc44cc296f551
thulanimtetwa <thulanimtetwa@gmail.com>
2026-03-24 23:34:14 +0200

    feat: add demo app, screenshots, and update README

---
+++ Examples/SecurityKitDemoApp/SecurityKitDemoApp/SecurityKitDemoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SecurityKitDemoAppApp.swift
+//  SecurityKitDemoApp
+//
+//  Created by Thulani Mtetwa on 2026/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SecurityKitDemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
